0967f2e5fa971fb045bad8f1d6f258d3f9f67215
Zach Schiff <zaschiff@gmail.com>
2021-12-16 10:38:14 -0700

    Completed Section 2

---
+++ Lists-Tuples-Dicts/assignment_01.py
@@ -0,0 +1,52 @@
+# Assignment 1:
+"""
+Print Bill's salary from the my_list object shown below.
+
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
+
+"""
+
+
+# your code below:
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
+print(my_list[0].get('Bill'))
+# print(my_list[0]['Bill']) this also works!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution
+# print(my_list[0].get('Bill'))
---
+++ Lists-Tuples-Dicts/assignment_02.py
@@ -0,0 +1,74 @@
+# Assignment 2:
+"""
+Using some of the collection data types we learned about
+in the course so such as a list and dictionary, create a
+data structure that best represents the following scenario:
+
+Tom has a salary of 20000 and is 22 years old. He owns a few items such as
+a jacket, a car, and TV. Mike is another person who makes 24000 and is 27 years old
+who owns a bike, a laptop and boat.
+"""
+
+# your code below:
+# option 1
+Tom = {'Salary': 20000, 'Age': 27, 'possesions': ['Jacket', 'Car', 'TV']}
+Mike = {'Salary': 24000, 'Age': 27, 'possesions': ['Bike', 'Laptop', 'Boat']}
+
+# option 2
+people = {'Tom': {'Salary': 20000, 'Age': 27, 'possesions': ['Jacket', 'Car', 'TV']},
+          'Mike': {'Salary': 24000, 'Age': 27, 'possesions': ['Bike', 'Laptop', 'Boat']}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution
+
+# my_list = [{'Tom': {'salary': 20000, 'age': 22, 'owns': ['jacket', 'car', 'TV']}},
+#            {'Mike': {'salary': 24000, 'age': 27, 'owns': ['bike', 'laptop', 'boat']}}]
